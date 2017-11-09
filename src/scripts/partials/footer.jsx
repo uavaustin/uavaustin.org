35bc8331394d1cf3b79f8928ced08c0aa69c2222
Bradley Bridges <blbridges96@gmail.com>
2017-11-08 18:32:50 -0600

    Making a simple footer at the bottom of the page

--- src/scripts/partials/footer.jsx
+++ src/scripts/partials/footer.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 export default class Footer extends React.Component {
     render() {
         return (
-            <div className='footer'>footer</div>
+            <footer>
+                <span>
+                    &copy; 2017 Unmanned Aerial Vehicle Team | UT&nbsp;Austin
+                </span>
+            </footer>
         );
     }
 }
